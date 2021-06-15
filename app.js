d358e4b5668a0f101383f4058581c581a77899b7
Armin <arminch1997@gmail.com>
2021-06-14 20:05:09 -0700

    code

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const runApp = () => {
     .then(res=>{
       if (res.action =='View All Employees') {showAllEmployees()}
       else if (res.action == "View All Employees By Role") {showEmpsByRole()}
+      else if (res.action == "View all Emplyees By Deparment") {showEmpsByDep()}
+      else if (res.action == "Add Employee") { addEmployee() }
     })
     .catch(err => console.log(err))
 
@@ -50,5 +52,55 @@ const showEmpsByRole = () => {
       })
     })
 })
+}
+const showEmpsByDep = () => {
+  db.query('SELECT name FROM employee_db.department', (err, deps)=>{
+    if (err) { console.log(err) }
+    let depArray=[]
+    for (let i = 0; i < deps.length; i++) { depArray.push(deps[i].name) }
+    prompt({
+      name: "dep",
+      type: "list",
+      message: "Which department would you like to search?",
+      choices: depArray
+    })
+    .then(res => {
+      db.query(`SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, m.last_name AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${res.dep}'`, (error, employees) => {
+        console.table(employees)
+        })
+      })
+  })
+}
+const addEmployee = () =>{
+  prompt([
+    {
+      name: "firstName",
+      type: "input",
+      message: "What is the first name of the new employee?"
+    },
+    {
+      name: "lastName",
+      type: "input",
+      message: "What is the last name of the new employee?"
+    },
+    {
+      name: "roleId",
+      type: "input",
+      message: "What is the role ID of the new employee?"
+    },
+    {
+      name: "managerId",
+      type: "input",
+      message: "What is the manager ID of the new employee?"
+    }
+  ])
+  .then(res=>{
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                VALUES ("${res.firstName}", "${res.lastName}", ${parseInt(res.roleId)}, ${parseInt(res.managerId)})`, (err,res2)=>{
+                  if (err) { console.log(err) }
+                  console.log('new employee added!')
+                  runApp()
+                })
+  })
 }
 runApp()
